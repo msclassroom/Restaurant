5ae5ead3b9e9ae1db405ee5aac0e7b2b1185b8b5
victor njonge <vinge1718@gmail.com>
2019-02-18 05:04:24 +0300

    added the restaurant activity to aid navigatio from one activity to another by use of intents

---
+++ app/src/main/java/io/github/vinge1718/restaurant/RestaurantsActivity.java
@@ -0,0 +1,13 @@
+package io.github.vinge1718.restaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+    }
+}
