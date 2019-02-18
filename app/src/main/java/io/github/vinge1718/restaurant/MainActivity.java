4f0f61e032383645f8a21f65cec744214dfa43f1
victor njonge <vinge1718@gmail.com>
2019-02-18 11:00:34 +0300

    added the editText view supposed to get the user input

--- app/src/main/java/io/github/vinge1718/restaurant/MainActivity.java
+++ app/src/main/java/io/github/vinge1718/restaurant/MainActivity.java
@@ -1,22 +1,30 @@
-package io.github.vinge1718.restaurant;
+package io.github.vinge1718.restaurant;import android.os.Bundle;
+
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-private Button mFindRestaurantsButton;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(MainActivity.this, "HelloWorld!", Toast.LENGTH_LONG).show();
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
+                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
             }
         });
     }
