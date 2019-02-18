1924de919abeba4de51912778ed8d4a00371444a
victor njonge <vinge1718@gmail.com>
2019-02-18 04:31:46 +0300

    added the most basic UI features of the restaurant app

---
+++ app/src/main/java/io/github/vinge1718/restaurant/MainActivity.java
@@ -0,0 +1,23 @@
+package io.github.vinge1718.restaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+private Button mFindRestaurantsButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "HelloWorld!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
