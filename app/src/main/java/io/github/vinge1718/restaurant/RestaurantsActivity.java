79cdefa9f020d8ddd618c8bb731d9e748d6e4195
victor njonge <vinge1718@gmail.com>
2019-02-19 15:03:18 +0300

    added arrayadapters to display array items on an a listview

--- app/src/main/java/io/github/vinge1718/restaurant/RestaurantsActivity.java
+++ app/src/main/java/io/github/vinge1718/restaurant/RestaurantsActivity.java
@@ -3,15 +3,38 @@ package io.github.vinge1718.restaurant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RestaurantsActivity extends AppCompatActivity {
     private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+           @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+               String restaurant = ((TextView)view).getText().toString();
+               Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+           }
+        });
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
