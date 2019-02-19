a2ecd44f5ad3985f146472334753a8b0caa051a3
victor njonge <vinge1718@gmail.com>
2019-02-19 10:32:45 +0300

    Used typecase to add font resources to the application

--- app/src/main/java/io/github/vinge1718/restaurant/MainActivity.java
+++ app/src/main/java/io/github/vinge1718/restaurant/MainActivity.java
@@ -1,23 +1,31 @@
-package io.github.vinge1718.restaurant;import android.os.Bundle;
+package io.github.vinge1718.restaurant;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
 
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.lang.reflect.Type;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAppNameTextView = findViewById(R.id.appNameTextView);
+        Typeface caviarFont = Typeface.createFromAsset(getAssets(), "fonts/CaviarDreams.ttf");
+        mAppNameTextView.setTypeface(caviarFont);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
