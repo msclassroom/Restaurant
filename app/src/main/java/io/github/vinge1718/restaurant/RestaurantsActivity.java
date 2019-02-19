bbebebf9b0b160cf74538b2ac2777f9ab74cb6fe
victor njonge <vinge1718@gmail.com>
2019-02-19 16:54:27 +0300

    editted the merge conflicts in this branch to all the rest

--- app/src/main/java/io/github/vinge1718/restaurant/RestaurantsActivity.java
+++ app/src/main/java/io/github/vinge1718/restaurant/RestaurantsActivity.java
@@ -18,23 +18,12 @@ public class RestaurantsActivity extends AppCompatActivity {
             "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
             "Lardo", "Portland City Grill", "Fat Head's Brewery",
             "Chipotle", "Subway"};
-<<<<<<< HEAD
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
 
-
-        mListView = (ListView) findViewById(R.id.listView);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
-        mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
-                String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
-            }
-        });
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mListView = (ListView) findViewById(R.id.listView);
@@ -50,8 +39,6 @@ public class RestaurantsActivity extends AppCompatActivity {
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
         mLocationTextView.setText("Here are all the restaurants near: " + location);
     }
-=======
->>>>>>> 5_arrays_and_lists
 
 
 }
