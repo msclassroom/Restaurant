29252533e241425b677a98d4b2a5ab3898cb4975
victor njonge <vinge1718@gmail.com>
2019-02-22 12:04:01 +0300

    added the yelp service class and the Okhttp callback in the restaurant class to fetch the restaurant details from the YELP API asynchronously

--- app/src/main/java/io/github/vinge1718/restaurant/RestaurantsActivity.java
+++ app/src/main/java/io/github/vinge1718/restaurant/RestaurantsActivity.java
@@ -3,6 +3,7 @@ package io.github.vinge1718.restaurant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,10 +11,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
+    public static final String TAG = RestaurantsActivity.class.getSimpleName();
     @BindView(R.id.locationTextView) TextView mLocationTextView;
     @BindView(R.id.listView) ListView mListView;
     private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
@@ -44,7 +51,28 @@ public class RestaurantsActivity extends AppCompatActivity {
             }
         });
         mLocationTextView.setText("Here are all the restaurants near: " + location);
+        getRestaurants(location);
     }
 
+    private void getRestaurants(String location){
+        final YelpService yelpService = new YelpService();
+        yelpService.findRestaurants(location, new Callback(){
+
+            @Override
+            public void onFailure(Call call, IOException e){
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try{
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
 }
---
+++ app/src/main/java/io/github/vinge1718/restaurant/YelpService.java
@@ -0,0 +1,26 @@
+package io.github.vinge1718.restaurant;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class YelpService {
+    public static void findRestaurants(String location, Callback callback){
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Authorization", Constants.YELP_TOKEN)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+}
