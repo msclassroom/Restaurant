d3e9dba32ecda95df41e1e2a422f8021fa5aa449
Vinge1718 <vinge1718@gmail.com>
2021-02-06 00:02:42 +0300

    added the recyclerview and it's corresponding viewholder and laout manager

---
+++ app/src/main/java/io/github/vinge1718/restaurant/adapters/RestaurantListAdapter.java
@@ -0,0 +1,65 @@
+package io.github.vinge1718.restaurant.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.github.vinge1718.restaurant.R;
+import io.github.vinge1718.restaurant.models.Business;
+
+public class RestaurantListAdapter  extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder> {
+    private List<Business> mRestaurants;
+    private Context mContext;
+
+    public RestaurantListAdapter(Context context, List<Business> restaurants) {
+        mContext = context;
+        mRestaurants = restaurants;
+    }
+
+    @Override
+    public RestaurantListAdapter.RestaurantViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_list_item, parent, false);
+        RestaurantViewHolder viewHolder = new RestaurantViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RestaurantListAdapter.RestaurantViewHolder holder, int position) {
+        holder.bindRestaurant(mRestaurants.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRestaurants.size();
+    }
+
+    public class RestaurantViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.restaurantImageView) ImageView mRestaurantImageView;
+        @BindView(R.id.restaurantNameTextView) TextView mNameTextView;
+        @BindView(R.id.categoryTextView) TextView mCategoryTextView;
+        @BindView(R.id.ratingTextView) TextView mRatingTextView;
+
+        private Context mContext;
+
+        public RestaurantViewHolder(View itemView){
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindRestaurant(Business restaurant) {
+            mNameTextView.setText(restaurant.getName());
+            mCategoryTextView.setText(restaurant.getCategories().get(0).getTitle());
+            mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+        }
+    }
+}
--- app/src/main/java/io/github/vinge1718/restaurant/ui/RestaurantsActivity.java
+++ app/src/main/java/io/github/vinge1718/restaurant/ui/RestaurantsActivity.java
@@ -1,6 +1,8 @@
 package io.github.vinge1718.restaurant.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,6 +19,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.github.vinge1718.restaurant.adapters.RestaurantListAdapter;
 import io.github.vinge1718.restaurant.models.Business;
 import io.github.vinge1718.restaurant.models.Category;
 import io.github.vinge1718.restaurant.MyRestaurantsArrayAdapter;
@@ -30,10 +33,11 @@ import retrofit2.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
     private static final String TAG = RestaurantsActivity.class.getSimpleName();
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.listView) ListView mListView;
+    private RestaurantListAdapter mAdapter;
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
     @BindView(R.id.errorTextView) TextView mErrorTextView;
     @BindView(R.id.progressBar) ProgressBar mProgressBar;
+    public List<Business> restaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +45,8 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
 
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
-            }
-        });
-
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
 
         YelpApi client = YelpClient.getClient();
         Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "restaurants");
@@ -63,22 +58,12 @@ public class RestaurantsActivity extends AppCompatActivity {
                 hideProgressBar();
 
                 if (response.isSuccessful()) {
-                    List<Business> restaurantsList = response.body().getBusinesses();
-                    String[] restaurants = new String[restaurantsList.size()];
-                    String[] categories = new String[restaurantsList.size()];
-
-                    for (int i = 0; i < restaurants.length; i++){
-                        restaurants[i] = restaurantsList.get(i).getName();
-                    }
-
-                    for (int i = 0; i < categories.length; i++) {
-                        Category category = restaurantsList.get(i).getCategories().get(0);
-                        categories[i] = category.getTitle();
-                    }
-
-                    ArrayAdapter adapter
-                            = new MyRestaurantsArrayAdapter(RestaurantsActivity.this, android.R.layout.simple_list_item_1, restaurants, categories);
-                    mListView.setAdapter(adapter);
+                    restaurants = response.body().getBusinesses();
+                    mAdapter = new RestaurantListAdapter(RestaurantsActivity.this, restaurants);
+                    mRecyclerView.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RestaurantsActivity.this);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setHasFixedSize(true);
 
                     showRestaurants();
                 } else {
@@ -106,8 +91,7 @@ public class RestaurantsActivity extends AppCompatActivity {
     }
 
     private void showRestaurants() {
-        mListView.setVisibility(View.VISIBLE);
-        mLocationTextView.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.VISIBLE);
     }
 
     private void hideProgressBar() {
