01e99092dc19dd3f648dad6bc724c2e645ff3e65
Vinge1718 <vinge1718@gmail.com>
2021-02-11 10:49:25 +0300

    added the click listener to the url, phone and map attributes of the fragment view

--- app/src/main/java/io/github/vinge1718/restaurant/ui/RestaurantDetailFragment.java
+++ app/src/main/java/io/github/vinge1718/restaurant/ui/RestaurantDetailFragment.java
@@ -1,5 +1,7 @@
 package io.github.vinge1718.restaurant.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -29,7 +31,7 @@ import io.github.vinge1718.restaurant.models.Category;
  * Use the {@link RestaurantDetailFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RestaurantDetailFragment extends Fragment {
+public class RestaurantDetailFragment extends Fragment implements View.OnClickListener{
     @BindView(R.id.restaurantImageView) ImageView mImageLabel;
     @BindView(R.id.restaurantNameTextView) TextView mNameLabel;
     @BindView(R.id.cuisineTextView) TextView mCategoriesLabel;
@@ -63,9 +65,10 @@ public class RestaurantDetailFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
+
         View view =  inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
         ButterKnife.bind(this, view);
         Picasso.get().load(mRestaurant.getImageUrl()).into(mImageLabel);
@@ -82,6 +85,28 @@ public class RestaurantDetailFragment extends Fragment {
         mPhoneLabel.setText(mRestaurant.getPhone());
         mAddressLabel.setText(mRestaurant.getLocation().toString());
 
+        mWebsiteLabel.setOnClickListener(this);
+        mPhoneLabel.setOnClickListener(this);
+        mAddressLabel.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mWebsiteLabel) {
+            Intent webIntent = new Intent (Intent.ACTION_VIEW, Uri.parse(mRestaurant.getUrl()));
+            startActivity(webIntent);
+        }
+        if (v == mPhoneLabel) {
+            Intent phoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + mRestaurant.getPhone()));
+            startActivity(phoneIntent);
+        }
+        if (v == mAddressLabel) {
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:" + mRestaurant.getCoordinates().getLatitude()
+                    + "," + mRestaurant.getCoordinates().getLongitude()
+                    + "?q=(" + mRestaurant.getName() + ")"));
+            startActivity(mapIntent);
+        }
     }
 }
