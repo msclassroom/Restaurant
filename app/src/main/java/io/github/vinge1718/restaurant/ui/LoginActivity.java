e198f8edd48eb83b2c133ec4df92097d042db397
Vinge1718 <vinge1718@gmail.com>
2021-02-22 19:30:35 +0300

    added the updated login and signup activites

---
+++ app/src/main/java/io/github/vinge1718/restaurant/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package io.github.vinge1718.restaurant.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.vinge1718.restaurant.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
